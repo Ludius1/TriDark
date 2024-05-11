354f24db77e43455f6180902da3748997692eb0a
Ludius1 <abubakarsholly002.com>
2024-05-11 18:35:09 +0100

    your Message

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -4,11 +4,19 @@ import { IoMenu } from "react-icons/io5";
 import { FiPlus } from "react-icons/fi";
 import { MdOutlineCancel } from "react-icons/md";
 import './navbar.css'
+import { useState } from 'react';
 
 
 
 
 function Navbar() {
+
+const [isOn, setIsOn] = useState(false)
+
+const handleMenu = () => {
+  setIsOn((prev)=>!prev)
+}
+
   return (
     <header>
       <nav>
@@ -18,25 +26,30 @@ function Navbar() {
             </a>
             
 
-            <ul className='nav__menu'> 
+            <ul className={ `nav__menu ${isOn ? 'show_nav' : ''}` }>
                 <li><a href="#hero">Home</a></li>
                 <li><a href="#listing">Rentals</a></li>
                 <li><a href="#categories">Categories</a></li>
                 <li><a href="#performance">About Us</a></li>
                 <li><a href="#footer">Contact</a></li>
                 <li><a href="#article">Blog</a></li>
-            </ul>
+</ul>
+
 
             <div className="right__nav">
                 <div className="sigin__cont">
                     <span id="sigin__icon"><FiUser/></span>
                     <h6>Sign in</h6>
                 </div>
+            </div>
 
-                <div className="menu__option">
-                <span id="menu" ><IoMenu /></span>
-                <span id="menu"><MdOutlineCancel /></span>
-                 </div>
+            <div className="last__nav">
+            <div className="menu__option">
+                  <span onClick={handleMenu}style={{opacity: 1}}
+                  id="menu"><IoMenu /></span>
+                  {/* <span onClick={handleExit}
+                  id="exit"><MdOutlineCancel /></span> */}
+            </div>
                 
                 <div className="post__listening">
                     <span id="FiPlus"><FiPlus/></span>
